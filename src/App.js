b31a75bab332ad9dc351f858a2032684f8d9115f
alucard87pl <mjedrecki@hotmail.com>
2019-04-05 21:41:43 +0200

    removed package-lock.json from .gitignore staged simple changes in App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ class App extends Component {
   render() {
     return (
       <div>
+        <h1>Starchasers: Chronicles</h1>
+        <hr/>
       <p>Legenda głosi, że Gwiazdogini istnieli od niepamiętnych czasów.</p>
       <p>Cóż... te kroniki zapewne powiedzą coś innego...</p>
       </div>
